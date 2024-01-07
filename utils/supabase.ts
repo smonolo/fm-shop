214bd4fe47657ce4c36fdcb0516cb209c5772a89
smonolo <lu3mtex@gmail.com>
2024-01-07 18:13:33 +0100

    feat(supabase): specify db schema

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -2,5 +2,10 @@ import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_KEY!,
+  {
+    db: {
+      schema: process.env.NEXT_PUBLIC_SUPABASE_SCHEMA,
+    },
+  }
 )
