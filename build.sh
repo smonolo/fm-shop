346f83beb2d575932d3e0b5dd51be98629ece6e6
smonolo <lu3mtex@gmail.com>
2024-01-24 13:35:28 +0100

    feat(ci): custom build step script for vercel

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "dev" || "$VERCEL_GIT_COMMIT_REF" == "main"  ]]; then
+  exit 1;
+else
+  exit 0;
+fi
