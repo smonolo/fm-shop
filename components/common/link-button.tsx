c0f3107085b916ec2700bfbaf89956baeda948d4
Leonardo Parisi <140561519+leonardoparisii@users.noreply.github.com>
2024-01-28 22:15:55 +0100

    Update components/common/link-button.tsx
    Co-authored-by: Stefano <lu3mtex@gmail.com>

--- components/common/link-button.tsx
+++ components/common/link-button.tsx
@@ -13,11 +13,7 @@ const LinkButton: FC<Props> = ({ href, text, className, children }) => {
     <a
       href={href}
       target="_blank"
-      className={` ${
-        className
-          ? className
-          : 'relative flex w-fit items-center gap-2 rounded-full bg-fm-aqua px-6 py-2.5 text-fm-dark shadow-fm-aqua transition-colors duration-500 hover:bg-fm-cyan'
-      }`}
+      className={className || 'relative flex w-fit items-center gap-2 rounded-full bg-fm-aqua px-6 py-2.5 text-fm-dark shadow-fm-aqua transition-colors duration-500 hover:bg-fm-cyan'}
     >
       {text ? (
         <>
