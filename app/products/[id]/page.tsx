241232d733be0775f5cd4a891fdae60050ef5184
Leonardo Parisi <parisii.leonardo>
2024-01-28 21:58:03 +0100

    navbar responsive, slightly customized linkedButton component

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -46,7 +46,7 @@ const Product: FC = () => {
   }
 
   return (
-    <section className="mx-auto w-full max-w-[1200px] pt-20">
+    <section className="mx-auto w-full max-w-[1400px] px-4 pt-20 sm:px-8 md:px-20">
       <div className="flex w-full items-center justify-between gap-4">
         <div className="flex w-fit items-center gap-5">
           <h1 className="text-[30px]/[35px] font-bold italic lg:text-[35px]/[40px]">
@@ -62,6 +62,7 @@ const Product: FC = () => {
             style: 'currency',
             currency: 'EUR',
           }).format(product.price)}`}
+          className="max-sm:hidden"
         />
       </div>
       <p className="mt-2 text-fm-aqua">
@@ -77,12 +78,21 @@ const Product: FC = () => {
         alt={product.name}
         width={2000}
         height={2000}
-        className="mt-10 max-h-[400px] w-full rounded-2xl object-cover"
+        className="mt-10 max-h-[400px] w-full select-none rounded-2xl object-cover"
+        draggable={false}
       />
-      <section className="mt-10 flex flex-col gap-2">
+      <section className="mt-10 flex flex-col gap-2 max-sm:mb-4 max-sm:mt-4">
         <h6 className="font-medium text-fm-aqua">About {product.name}</h6>
         <p>{product.description}</p>
       </section>
+      <LinkButton
+        href={product.shopLink}
+        text={`Buy ${Intl.NumberFormat(navigator.language, {
+          style: 'currency',
+          currency: 'EUR',
+        }).format(product.price)}`}
+        className="w-full justify-center sm:hidden"
+      />
     </section>
   )
 }
