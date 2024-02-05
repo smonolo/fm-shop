88e2625d360b6f578dd801a3830b6cf1555b0ef8
Leonardo Parisi <parisii.leonardo>
2024-02-05 23:15:38 +0100

    feat: scrollbar and cards faq page

--- components/faq/card.tsx
+++ components/faq/card.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import type { FC } from 'react'
-import Link from 'next/link'
 import { FAQ } from '@/types/faq'
 
 type Props = {
@@ -10,17 +9,16 @@ type Props = {
 
 const FAQCard: FC<Props> = ({ faq }) => {
   return (
-    <Link
-      href={`/products/${faq.id}`}
-      className="group mx-auto flex w-full cursor-pointer items-center overflow-hidden rounded-3xl border-4 border-solid border-fm-dark/60 bg-cover bg-center backdrop-blur md:w-[80%]"
-    >
-      <div className="flex h-[120px] w-full items-center justify-between gap-4 bg-fm-dark/70 px-16 py-8 transition-[height] delay-[100ms] duration-[400ms] hover:h-[200px]">
-        <div className="flex w-fit items-baseline gap-2">
-          <h3 className="text-lg font-semibold lg:text-2xl">{faq.question}</h3>
-          <h3 className="text-lg font-semibold lg:text-2xl">{faq.answer}</h3>
+    <div className="group mx-auto flex w-full cursor-pointer items-center overflow-hidden rounded-3xl border-4 border-solid border-fm-dark/60 bg-cover bg-center backdrop-blur md:w-[80%]">
+      <div className="flex w-full items-center justify-between gap-4 bg-fm-dark/70 px-16 py-8">
+        <div className="flex w-fit flex-col items-baseline gap-2">
+          <h3 className="text-2xl font-semibold">{faq.question}</h3>
+          <h3 className="text-base font-medium text-fm-light/80">
+            {faq.answer}
+          </h3>
         </div>
       </div>
-    </Link>
+    </div>
   )
 }
 
